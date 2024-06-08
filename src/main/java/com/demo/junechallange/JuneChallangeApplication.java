93e1901ba02536f7686307f63d8c0307bcc8511c
kimHaneul0202 <kimsky1@douzone.com>
2024-06-08 15:34:21 +0900

    Remove embedded Git repository

---
+++ src/main/java/com/demo/junechallange/JuneChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.junechallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuneChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuneChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/junechallange/JuneChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.junechallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuneChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
